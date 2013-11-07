0688a06258da4945968053f05d78f64a6f1172e4
brittlewis12 <brittlewis12@gmail.com>
2013-11-07 17:44:33 -0500

    beatcove initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beatcove::Application.config.secret_key_base = '8ac3e949a4866a8b50246ad28d50972d6a54f2cfe1d9a076dac90b73bcd49c3169d57b4c7f964c6b5ba08a3b48a7abf3e770a94069aa73b7090aabadc1589691'
