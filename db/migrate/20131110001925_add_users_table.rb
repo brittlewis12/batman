d3b509d318d7d09d59490a72f2ea1ad434f8078b
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-10 01:03:05 -0500

    added usernames to users table and attempting to git ignore database.yml

--- db/migrate/20131110001925_add_users_table.rb
+++ db/migrate/20131110001925_add_users_table.rb
@@ -1,6 +1,7 @@
 class AddUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username, null:false
       t.string :email, null: false
       t.string :password_digest, null: false
       t.timestamps
