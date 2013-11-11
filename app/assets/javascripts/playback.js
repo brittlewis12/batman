edd9d488dc927c4b1473172d090f3f3375e33174
teabait <hello.teaho@gmail.com>
2013-11-11 12:13:48 -0500

    Multiple playback hard coded

--- app/assets/javascripts/playback.js
+++ app/assets/javascripts/playback.js
@@ -1,14 +1,66 @@
 var context;
 var bufferLoader;
 
+// buffer Loader Class 
+  function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
+}
+
+// end buffer loader class
+
 $(function(){
+
+
   $('button').on('click', function(){
-    init();
+    initLoader();
   });
 })
 
 
-function init() {
+function initLoader() {
   // Fix up prefixing
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   context = new AudioContext();
@@ -16,8 +68,8 @@ function init() {
   bufferLoader = new BufferLoader(
     context,
     [
-      'http://allmuz.org/audio/223863373/167444781/Miley_Cyrus-Wrecking_Ball_rington',
-      'http://media.sparke.cn/audio/201008131157361.mp3',
+      'https://dl.dropboxusercontent.com/s/yk6lk7veq0xroa7/05-ho-hey.mp3?dl=1&token_hash=AAG1FslNPq956S9dr3peWLn1w7YFmQ6vK90e092M-DumAA',
+      'https://dl.dropboxusercontent.com/s/jqijit6n4byl57v/Miley_Cyrus-Wrecking_Ball_rington.mp3?dl=1&token_hash=AAG3luOoVZO-v0BMjblCh8huimvOGsq7B0u0vjAiNmvLYg',
     ],
     finishedLoading
     );
