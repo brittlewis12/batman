b8171f7d40c2dc0fddd7f584b83f9387733e064b
teabait <hello.teaho@gmail.com>
2013-11-11 11:41:57 -0500

    playback.js

---
+++ app/assets/javascripts/playback.js
@@ -0,0 +1,37 @@
+var context;
+var bufferLoader;
+
+$(function(){
+  $('button').on('click', init);
+})
+
+
+function init() {
+  // Fix up prefixing
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  context = new AudioContext();
+
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      'http://allmuz.org/audio/223863373/167444781/Miley_Cyrus-Wrecking_Ball_rington',
+      'http://media.sparke.cn/audio/201008131157361.mp3',
+    ],
+    finishedLoading
+    );
+
+  bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+  // Create two sources and play them both together.
+  var source1 = context.createBufferSource();
+  var source2 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
+
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source1.start(0);
+  source2.start(0);
+}
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,5 @@
 class SongsController < ApplicationController
-  def index; end
+  def index
+    render :index
+  end
 end
