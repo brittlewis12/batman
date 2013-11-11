7ac6920a0752b635ea51e6cee8de5d453483747b
teabait <hello.teaho@gmail.com>
2013-11-11 11:21:08 -0500

    fixed indentation and add LOG for Friday

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Beatcove::Application.routes.draw do
-<<<<<<< HEAD
-resources :songs, only: [:index]
-=======
   resources :songs, only: [:index]
->>>>>>> 280c0c4990372ac928808086072614b701e43a6a
 end
