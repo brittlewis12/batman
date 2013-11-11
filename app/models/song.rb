2ff95f0ec48d8178760c24e05ed2de377df7fedd
brittlewis12 <brittlewis12@gmail.com>
2013-11-11 11:31:46 -0500

    fixed spacing quirk in songs model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
 
-  validates :name, :creator_id, presence:true
+  validates :name, :creator_id, presence: true
 
   has_many :tracks
   has_many :users, through: :tracks
