beb30ccbf2d603abb935c270c26dc9f07851b0ba
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-11 15:24:09 -0500

    fleshing out controllers with bare-bones views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,49 @@
 class UsersController < ApplicationController
 
-def index
-end
+  before_action :set_user, except: [:new, :create]
 
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      redirect_to gjhkl
+    end
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+  
+  end
+
+
+  def destroy
+
+  end
+
+  def show
+    render :show
+  end
+
+
+
+
+ private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
