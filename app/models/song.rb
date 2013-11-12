b788bdb4d5f638982bb0c1643e6a097814871afe
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-12 16:45:03 -0500

    made collaborators controller(collaborators has no model, or table. fancy that...)

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :name, :creator_id, presence: true
   has_many :tracks
-  has_many :users, through: :tracks
+  has_and_belongs_to_many :users
   belongs_to :creator, class_name: :User, foreign_key: :creator_id
 end
