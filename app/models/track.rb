bb87d55143d3a85ad1854e57d808dcece49c1fd5
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-14 13:50:39 -0500

    added default (false) for 'self.removed' in track model, track persistence now works

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,13 @@
 class Track < ActiveRecord::Base
-  validates :name, :url, :song_id, :user_id, :removed, presence: true
+  after_initialize :set_removed
+
+  validates :name, :url, :song_id, :user_id, presence: true
   validates :url, uniqueness: true
 
   belongs_to :user
   belongs_to :song
+  
+  def set_removed
+    self.removed ||= false
+  end
 end
