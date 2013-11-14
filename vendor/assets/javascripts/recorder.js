fdd9cb9384e5bdb242c506bb28dde02d14cdb915
brittlewis12 <brittlewis12@gmail.com>
2013-11-14 10:32:09 -0500

    working toward consistent spacing and code style

--- vendor/assets/javascripts/recorder.js
+++ vendor/assets/javascripts/recorder.js
@@ -8,7 +8,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-(function(window){
+( function( window ){
 
   var WORKER_PATH = '/assets/recorderWorker.js';
 
@@ -28,7 +28,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     var recording = false,
       currCallback;
 
-    this.node.onaudioprocess = function(e){
+    this.node.onaudioprocess = function(e) {
       if (!recording) return;
       worker.postMessage({
         command: 'record',
@@ -39,7 +39,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
       });
     }
 
-    this.configure = function(cfg){
+    this.configure = function(cfg) {
       for (var prop in cfg){
         if (cfg.hasOwnProperty(prop)){
           config[prop] = cfg[prop];
@@ -132,4 +132,4 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
   window.Recorder = Recorder;
 
-})(window);
+} )(window);
