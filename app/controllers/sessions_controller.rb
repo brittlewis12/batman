2ea73ca45c3b82d9f80c36c8c4cc55da8c295f4a
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-14 11:19:00 -0500

    if login unsuccessful render new (session)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
+    else
+      render :new
     end
   end
 
