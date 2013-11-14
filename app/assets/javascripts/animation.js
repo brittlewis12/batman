a7cc1f4742568589572b42b669f5e1c9dfdeb2cc
teabait <hello.teaho@gmail.com>
2013-11-14 16:34:13 -0500

    pushing all of the current changes with hidden about

---
+++ app/assets/javascripts/animation.js
@@ -0,0 +1,20 @@
+$(function(){
+  // slide down
+  $("#about-link").click(function(){
+    $("#about").slideDown("slow");
+  });
+  
+  // // smooth scrolling
+  // $('a[href*=#]:not([href=#])').click(function() {
+  //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //     if (target.length) {
+  //       $('html,body').animate({
+  //         scrollTop: target.offset().top
+  //       }, 1000);
+  //       return false;
+  //     }
+  //   }
+  // });
+});
