d764226d75ff6a06ada3fe1f45fa2dff086913de
brittlewis12 <brittlewis12@gmail.com>
2013-11-14 13:35:11 -0500

    new migration for adding removed boolean col to tracks; updated AR validations; rather than delete the entry from our tracks table upon tracks#destroy, we simply change the boolean value to true. other misc changes

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,5 @@
 class Track < ActiveRecord::Base
-  validates :name, :url, :song_id, :user_id, presence: true
+  validates :name, :url, :song_id, :user_id, :removed, presence: true
   validates :url, uniqueness: true
 
   belongs_to :user
